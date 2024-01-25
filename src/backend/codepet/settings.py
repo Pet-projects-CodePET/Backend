4d24d630a4d6d53b2c8faeaa212b20d16e1a297c
Kozin <grot_artem@inbox.ru>
2024-01-25 18:32:57 +0300

    Added the basic structure of the project

---
+++ src/backend/codepet/settings.py
@@ -0,0 +1,111 @@
+from os import getenv
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+DEBUG = getenv("DEBUG", default="False") == "True"
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "codepet.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "codepet.wsgi.application"
+
+if getenv("USE_SQLITE", default="True") == "True":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": getenv(
+                "DB_ENGINE", default="django.db.backends.postgresql"
+            ),
+            "NAME": getenv("DB_NAME", default="test"),
+            "USER": getenv("POSTGRES_USER", default="test_user"),
+            "PASSWORD": getenv("POSTGRES_PASSWORD", default="test_password"),
+            "HOST": getenv("DB_HOST", default="db"),
+            "PORT": getenv("DB_PORT", default=5432),
+        }
+    }
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "static/"
+STATIC_ROOT = Path(BASE_DIR, "collected_static")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = Path(BASE_DIR, "media")
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
