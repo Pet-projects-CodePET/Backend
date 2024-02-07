add996100a4eda2fb669758350b660965e1fd627
Olesya Orlenko <olees.orlenko@yandex.ru>
2024-02-07 19:16:33 +0300

    refactor: move project_app to the apps

--- src/backend/apps/project/apps.py
+++ src/backend/apps/project/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ProjectConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "project"
+    name = "apps.project"
--- src/backend/config/settings/base.py
+++ src/backend/config/settings/base.py
@@ -31,7 +31,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS: list = [
     "apps.general",
-    "project",
+    "apps.project",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
