5d06cceb26ea9c72d3f64960b7083d8a0f4af75a
Nadya2502 <fornmer@yandex.ru>
2024-02-09 15:19:28 +0100

    В приложении general создала модель текстовых элементов главной страницы

--- src/backend/apps/general/models.py
+++ src/backend/apps/general/models.py
@@ -9,3 +9,17 @@ class CreatedModifiedFields(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Section(models.Model):
+    """Секции на главной странице"""
+
+    title = models.TextField(
+        verbose_name="Заголовок", max_length=55, null=False
+    )
+    description = models.TextField(
+        verbose_name="Текст", max_length=160, null=False
+    )
+
+    def __str__(self):
+        return self.title
