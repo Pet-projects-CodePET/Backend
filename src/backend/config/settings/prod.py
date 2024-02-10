14f2b7000062857fb28ae426daa56a996f04e7e1
shoxxd221 <shoxxd@mail.ru>
2024-02-10 21:15:55 +0300

    Fix run black

--- src/backend/config/settings/prod.py
+++ src/backend/config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 DEBUG = False
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
