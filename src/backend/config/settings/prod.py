19945baa3ce6ae716e6f627f493378fb16cfa81d
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2024-03-13 19:50:26 +0300

    added cors settings

--- src/backend/config/settings/prod.py
+++ src/backend/config/settings/prod.py
@@ -6,3 +6,10 @@ DEBUG = False
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
+
+CORS_ALLOWED_ORIGINS = [
+    "http://89.23.117.80:80",
+    "http://devcodepet.tw1.ru:80",
+    "http://89.23.117.168:80",
+    "http://testcodepet.tw1.ru:80",
+]
