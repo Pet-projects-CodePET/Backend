a70cdb293e41aeb1e5ea1c052444ae3b6fd135b0
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2024-03-13 19:53:27 +0300

    saved base and dev . . .

--- src/backend/config/settings/base.py
+++ src/backend/config/settings/base.py
@@ -197,10 +197,3 @@ SPECTACULAR_SETTINGS = {
     },
     "COMPONENT_SPLIT_REQUEST": True,
 }
-
-CORS_ALLOWED_ORIGINS = [
-    "http://89.23.117.80:80",
-    "http://devcodepet.tw1.ru:80",
-    "http://89.23.117.168:80",
-    "http://testcodepet.tw1.ru:80",
-]
--- src/backend/config/settings/dev.py
+++ src/backend/config/settings/dev.py
@@ -3,3 +3,10 @@ from .base import *  # noqa
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
+
+CORS_ALLOWED_ORIGINS = [
+    "http://89.23.117.80:80",
+    "http://devcodepet.tw1.ru:80",
+    "http://89.23.117.168:80",
+    "http://testcodepet.tw1.ru:80",
+]
