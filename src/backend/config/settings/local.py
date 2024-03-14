f4ec07b99f859580e09b8c098d0a27ef6c4c91bc
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2024-03-14 19:27:50 +0300

    fixed cors

--- src/backend/config/settings/local.py
+++ src/backend/config/settings/local.py
@@ -59,5 +59,4 @@ LOGGING = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
 ]
--- src/backend/config/settings/prod.py
+++ src/backend/config/settings/prod.py
@@ -8,8 +8,6 @@ EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 
 CORS_ALLOWED_ORIGINS = [
-    "http://89.23.117.80:80",
-    "http://devcodepet.tw1.ru:80",
-    "http://89.23.117.168:80",
+    "http://89.23.117.168:3000",
     "http://testcodepet.tw1.ru:80",
 ]
